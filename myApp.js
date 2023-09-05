6e41a7bf2d9bd8a9685911998e50837acd727db8
Iryna Litvinova <lytvynlysko@gmail.com>
2023-09-05 12:42:41 +0300

    Create a person schema called personSchema. Create a model from the personSchema and assign it to the existing variable Person

--- myApp.js
+++ myApp.js
@@ -3,8 +3,14 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const Schema = mongoose.Schema;
+const personSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String]
+});
 
-let Person;
+const Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
