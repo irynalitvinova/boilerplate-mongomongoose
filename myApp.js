ae07a1f4f270b5e7277b20fceb1c942c90b7c8e7
Iryna Litvinova <lytvynlysko@gmail.com>
2023-09-05 19:36:33 +0300

    Modify the findPersonById to find the only person having a given _id, using Model.findById() -> Person. Use the function argument personId as the search key.

--- myApp.js
+++ myApp.js
@@ -61,9 +61,17 @@ const findOneByFood = function (food, done) {
   });
 };
 
-const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+const findPersonById = function (personId, done) {
+  Person.findById(personId, function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    else {
+      done(null, data);
+    }
+  });
 };
+
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
